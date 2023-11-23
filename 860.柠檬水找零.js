bf7bf936c346266bb22f3b8a93086b92cf3e6112
Shadows-kk <1213099875@qq.com>
2023-11-23 17:27:30 +0800

    柠檬水找零

--- 860.柠檬水找零.js
+++ 860.柠檬水找零.js
@@ -9,8 +9,32 @@
  * @param {number[]} bills
  * @return {boolean}
  */
-var lemonadeChange = function(bills) {
-
+var lemonadeChange = function (bills) {
+  let fiveNums = 0;
+  let tenNums = 0;
+  for (let i = 0; i < bills.length; i++) {
+    const bill = bills[i];
+    if (bill === 5) {
+      fiveNums++;
+    } else if (bill === 10) {
+      if (fiveNums > 0) {
+        fiveNums--;
+        tenNums++;
+      } else {
+        return false;
+      }
+    } else {
+      // 20
+      if (tenNums > 0 && fiveNums > 0) {
+        tenNums--;
+        fiveNums--;
+      } else if (fiveNums >= 3) {
+        fiveNums -= 3;
+      } else {
+        return false;
+      }
+    }
+  }
+  return true;
 };
 // @lc code=end
-
