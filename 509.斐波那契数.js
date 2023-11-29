014390f62f8b810576ba37731cc682bcdc32f6b1
Shadows-kk <1213099875@qq.com>
2023-11-29 17:34:32 +0800

    斐波那契数列

---
+++ 509.斐波那契数.js
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=509 lang=javascript
+ *
+ * [509] 斐波那契数
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function (n) {
+  // 方式一：递归方式
+  // if (n <= 1) {
+  //   return n;
+  // } else {
+  //   return fib(n - 1) + fib(n - 2);
+  // }
+  // 方式二
+  //  确定dp[i] = dp[i-1] + dp[i-2]
+  // 初始值 [0,1]
+  let dp = [0, 1];
+  for (let i = 2; i <= n; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2];
+  }
+  return dp[n];
+};
+// @lc code=end
