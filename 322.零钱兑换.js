eb9d864f78195c143757a26e908345d447bf9ad9
Shadows-kk <1213099875@qq.com>
2023-12-04 17:32:50 +0800

    零钱兑换

---
+++ 322.零钱兑换.js
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=322 lang=javascript
+ *
+ * [322] 零钱兑换
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+var coinChange = function (coins, amount) {
+  if (!amount) return 0;
+  let dp = Array(amount + 1).fill(Infinity);
+  dp[0] = 0;
+  for (let i = 0; i < coins.length; i++) {
+    for (let j = coins[i]; j <= amount; j++) {
+      dp[j] = Math.min(dp[j - coins[i]] + 1, dp[j]);
+    }
+  }
+  return dp[amount] === Infinity ? -1 : dp[amount];
+};
+// @lc code=end
--- 509.斐波那契数.js
+++ 509.斐波那契数.js
@@ -16,13 +16,28 @@ var fib = function (n) {
   // } else {
   //   return fib(n - 1) + fib(n - 2);
   // }
+
   // 方式二
   //  确定dp[i] = dp[i-1] + dp[i-2]
   // 初始值 [0,1]
-  let dp = [0, 1];
-  for (let i = 2; i <= n; i++) {
-    dp[i] = dp[i - 1] + dp[i - 2];
-  }
-  return dp[n];
+  // let dp = [0, 1];
+  // for (let i = 2; i <= n; i++) {
+  //   dp[i] = dp[i - 1] + dp[i - 2];
+  // }
+  // return dp[n];
+
+  // 方式三 增加一个备忘录，缓存中间的计算结果
+  let memo = [];
+  return helper(memo, n);
 };
+function helper(memo, n) {
+  if (n <= 1) {
+    return n;
+  }
+  if (memo[n]) {
+    return memo[n];
+  }
+  memo[n] = helper(memo, n - 1) + helper(memo, n - 2);
+  return memo[n];
+}
 // @lc code=end
